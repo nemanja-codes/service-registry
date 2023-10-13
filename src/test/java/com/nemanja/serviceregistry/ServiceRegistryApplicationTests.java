91aacf74e9260a208362899950ef0f955aa2db5b
Nemanja Markovic <necamark@gmail.com>
2023-10-13 23:44:57 +0200

    Napravljen Eureka server

---
+++ src/test/java/com/nemanja/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemanja.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
